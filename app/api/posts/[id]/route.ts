83f2e5643dc81d3f4785bb90d4749d6ab8bbc74d
linhhtm <linh.hoangmy12@gmail.com>
2024-02-23 16:38:52 +0700

    t2c2

--- app/api/posts/[id]/route.ts
+++ app/api/posts/[id]/route.ts
@@ -10,7 +10,7 @@ export async function GET(req: Request) {
     ? NextResponse.json(filtered[0])
     : NextResponse.json(
         {
-          message: `Blog with id: ${id} 22`,
+          message: `Blog with id: ${id} 11`,
         },
         {
           status: 22,
