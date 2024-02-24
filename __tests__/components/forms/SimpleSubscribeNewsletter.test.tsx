259b740d91b5cba4a1de8ed80d1f1e10a4e0afd1
linhhtm <linh.hoangmy12@gmail.com>
2024-02-24 12:57:34 +0700

    fix test

--- __tests__/components/forms/SimpleSubscribeNewsletter.test.tsx
+++ __tests__/components/forms/SimpleSubscribeNewsletter.test.tsx
@@ -1,22 +1,22 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+import { act, fireEvent, render, screen } from "@testing-library/react";
 import Page from "components/forms/SimpleSubscribeNewsletter";
 
 describe("Page", () => {
   const subscribe = jest.fn();
   render(<Page subscribe={subscribe} />);
-  it("renders page and submit button", () => {
+  it("renders page and submit button", async () => {
     const input = screen.getByRole("textbox", { name: /newsletter/i });
     expect(input).toBeInTheDocument();
     fireEvent.input(input, {
-      value: 'linh@gmail.com'
-    })
+      value: "linh@gmail.com",
+    });
 
-    const button = screen.getByRole("button", { name: /Subscribe Now 2/i });
+    const button = screen.getByRole("button", { name: /Subscribe Now/i });
     expect(button).toBeInTheDocument();
-    fireEvent.click(button)
-    expect(subscribe).toHaveBeenCalledTimes(1)
+    await act(async () => {
+      fireEvent.click(button);
+    });
+    expect(subscribe).toHaveBeenCalledTimes(1);
   });
-  it("subscribe", () => {
-  
-  })
+  it("subscribe", () => {});
 });
